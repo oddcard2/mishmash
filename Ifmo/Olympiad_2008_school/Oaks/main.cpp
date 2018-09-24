6b08e75df78e1c197ef2dbe3b9c363c0310e9620
iamoddcard <oddcard@yandex.ru>
2018-09-24 16:57:46 +0500

    Oaks initial project

---
+++ Ifmo/Olympiad_2008_school/Oaks/main.cpp
@@ -0,0 +1,29 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+string input = "";
+
+int main(int argc, char **argv) {
+	cout.sync_with_stdio(false);
+	cin.sync_with_stdio(false);
+
+	istream *pinp;
+#ifndef ONLINE_JUDGE
+	unique_ptr<istream> stream;
+	if (argc > 1) {
+		pinp = new ifstream(argv[1]);
+		stream.reset(pinp);
+	}
+	else if (!input.empty()) {
+		pinp = new istringstream(input);
+		stream.reset(pinp);
+	} else
+#endif	
+	{
+		pinp = &cin;
+	}
+	istream &icp = *pinp;
+	
+	return 0;
+}
