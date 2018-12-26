4c9b2de252abf919910e4afc3d32683dd9fca10f
iamoddcard <oddcard@yandex.ru>
2018-12-26 16:22:09 +0500

    new empty files added for futher algorithms

---
+++ Algorithms/geometry/max_triangle_square.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+
+//btw good article (n^2 and nlogn) - https://arxiv.org/pdf/1705.11035.pdf
+
+using namespace std;
+
+namespace {
+	
+
+
+	TEST(max_triangle_square, simple_test) {
+
+	}
+
+}
+
