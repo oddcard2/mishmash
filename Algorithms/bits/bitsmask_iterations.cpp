dbeee9cf6429b7b75cda516ce6c25c9aa2f424c2
oddcard_hp <oddcard@yandex.ru>
2019-11-18 00:21:45 +0500

    bitmasks dp staff added

--- Algorithms/bits/bitsmask_iterations.cpp
+++ Algorithms/bits/bitsmask_iterations.cpp
@@ -2,9 +2,25 @@
 #include <common.h>
 
 // increasing order, useful for DP
-//for (int i = 1; i < (1 << 8); i++) {
-//
-//}
+template<typename T>
+void init_bits(vector<T>& vals, vector<T>& bit_vals, int bits_count, int offset = 0) {
+	for (T b = 0; b < (T)bits_count; b++) {
+		vals[T(1) << b] = bit_vals[b + offset];
+	}
+}
+
+template<typename T>
+void mask_recalc(int mask_len, vector<T>& vals) {
+	T all_bits_mask = ((T(1) << mask_len) - 1);
+	for (T v = 1; v < (T(1) << mask_len); v++) {
+		for (T b = 0; b < (T)mask_len; b++) {
+			if (v & (T(1) << b)) {
+				vals[v] = vals[(v & ~(T(1) << b)) & all_bits_mask] + vals[(T(1) << b)];
+				break;
+			}
+		}
+	}
+}
 
 // all submasks of the mask - http://e-maxx.ru/algo/all_submasks
 
